bd66fbfca1b832b51adfeb5877a97426d6eaca45
rafaelhernandezrios <edgar_hdez94@hotmail.com>
2025-12-04 15:00:11 +0900

    transcription in deploy

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -14,8 +14,13 @@ import axios from "axios";
 import dotenv from "dotenv";
 import path from "path";
 import fs from "fs";
+import { fileURLToPath } from "url";
 
 dotenv.config();
+
+// Definir __dirname para ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const router = express.Router();
 
